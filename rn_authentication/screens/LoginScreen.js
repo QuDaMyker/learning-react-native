21ed142c3604f831d9632e124deb66001acdc64f
VN The Boss Baby <21520178@gm.uit.edu.vn>
2024-01-27 14:15:29 +0700

    update

---
+++ rn_authentication/screens/LoginScreen.js
@@ -0,0 +1,30 @@
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser, login } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
+
+function LoginScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
+  async function loginHandler(email, password) {
+    setIsAuthenticating(true);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not log you in. Please try again"
+      );
+    }
+    setIsAuthenticating(false);
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logining you in..." />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
+}
+
+export default LoginScreen;
